4c7e53840cd759316f0dbcd0fff70c16920ddbc9
Cauã Andrade <caua.goldenboyy@gmail.com>
2025-03-29 02:34:44 -0300

    add fetch code

--- findereletricstation/src/App.jsx
+++ findereletricstation/src/App.jsx
@@ -3,6 +3,36 @@ import './App.scss';
 
 function App() {
 
+
+  const axios = require('axios');
+
+// Definindo os parâmetros da requisição
+const options = {
+  method: 'GET',
+  url: 'https://ev-charge-finder.p.rapidapi.com/search-by-location',
+  params: {
+    near: 'São Paulo, SP, Brasil', // Localização de busca no Brasil
+    limit: '20' // Limite de resultados
+  },
+  headers: {
+    'x-rapidapi-key': 'bf8dff4de0mshf38228eec688b4cp172b86jsn313bc7b1a56d',
+    'x-rapidapi-host': 'ev-charge-finder.p.rapidapi.com'
+  }
+};
+
+// Função para buscar dados
+async function fetchData() {
+  try {
+    const response = await axios.request(options);
+    console.log('Postos de Energia:', response.data);
+  } catch (error) {
+    console.error('Erro na Requisição:', error.response ? error.response.data : error.message);
+  }
+}
+
+fetchData();
+
+
   const[location,setLocation] = useState('')
 
   
